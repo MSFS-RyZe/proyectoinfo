a5dca2831153f3101d88bf39473c4599cc2c7518
sbaro0 <sergi.baro@estudiantat.upc.edu>
2025-04-24 12:51:47 +0200

    Update path.py

--- PROYECTO/path.py
+++ PROYECTO/path.py
@@ -1,1 +1,18 @@
+class Path:
+    def __init__(self, nodes=[]):
+        self.nodes = nodes[:]  # list of Node objects
+        self.real_cost = 0.0
+
+    def AddNode(self, node, cost):
+        self.nodes.append(node)
+        self.real_cost += cost
+
+    def LastNode(self):
+        return self.nodes[-1]
+
+    def ContainsNode(self, node):
+        return node in self.nodes
+
+    def EstimatedTotalCost(self, dest_node):
+        return self.real_cost + Distance(self.LastNode(), dest_node)
 
