29e22b8a3cbe8e9b43dc3e15d6475adffd874757
adam <65952325+MSFS-RyZe@users.noreply.github.com>
2025-05-27 18:36:22 +0200

    Update navPoint.py

--- proyecto_completo_v1_v2_v3/navPoint.py
+++ proyecto_completo_v1_v2_v3/navPoint.py
@@ -4,6 +4,19 @@ class NavPoint:
         self.name = name
         self.latitude = float(latitude)
         self.longitude = float(longitude)
+        
+        # Compatibilidad con el sistema de grafos
+        self.x = self.longitude
+        self.y = self.latitude
+        self.neighbors = []
 
     def __repr__(self):
         return f"NavPoint({self.number}, {self.name}, {self.latitude}, {self.longitude})"
+
+    def __eq__(self, other):
+        if isinstance(other, NavPoint):
+            return self.number == other.number
+        return False
+
+    def __hash__(self):
+        return hash(self.number)
