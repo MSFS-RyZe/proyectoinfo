1c6b0594d74b25596a7cddd1aa4dc3d0e7ceb540
adam <65952325+MSFS-RyZe@users.noreply.github.com>
2025-05-29 15:40:47 +0200

    Delete proyecto_completo_v1_v2_v3(((((123123))))) directory

--- proyecto_completo_v1_v2_v3(((((123123)))))/navSegment.py
+++
@@ -1,8 +0,0 @@
-class NavSegment: #Representa conexiones o segmentos numerados
-    def __init__(self, origin_number, destination_number, distance):
-        self.origin_number = int(origin_number) #Número del nodo de origen
-        self.destination_number = int(destination_number) #Número del nodo del destino
-        self.distance = float(distance) #Distancia o coste entre los nodos
-
-    def __repr__(self): #Define como se representa el segmento al ser mostrado
-        return f"NavSegment({self.origin_number} -> {self.destination_number}, {self.distance})"
